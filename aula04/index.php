273a3d87ee2c0c30ae18efb9fdb269e5c9c45bbe
HiagoLayann <47778635+HiagoLayann@users.noreply.github.com>
2019-05-15 21:52:02 -0300

    Aula Métodos GET e POST - 15/05
    Aula Métodos GET e POST - 15/05

---
+++ aula04/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    
+        <meta charset="utf-8">
+        <title> Example Methood Post/Get</title>
+
+    </head>
+
+    <body>
+
+        <form name="cadastro" action="cadastro.php" methof="post">
+            <div>
+                <label>Nome:</label>
+                <input type="text" name="nome" id="nome"  />
+            </div>
+
+            <div>
+                <label>Email:</label>
+                <input type="email" name="email" id="email"  />
+          
+            </div>
+            
+            <div>
+                <label>Senha:</label>
+                <input type="password" name="senha" id="senha"  />
+            </div>
+
+            <div>
+                <label>Observação:</label>
+                <textarea  name="observacao" id="observacao"></textarea>
+            </div>
+            
+            <div>
+                <input id="btEnviar" type="submit" value="cadastrar" />
+            
+            </div>
+
+        </form>        
+
+    </body>
+
+</html>
